d489b1985edc19143ac6b573d8cdf00a6b50895c
jgmp19 <jgmp11@gmail.com>
2021-06-13 00:35:34 -0300

    API Pronta para uso

--- src/main/java/com/example/demo/Controller/ClienteController.java
+++ src/main/java/com/example/demo/Controller/ClienteController.java
@@ -2,9 +2,16 @@ package com.example.demo.Controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +30,29 @@ public class ClienteController {
 	}
 	
 	@PostMapping("/cliente")
-	public Cliente saveUsuario(@RequestBody Cliente cliente) {
+	public Cliente saveCliente(@RequestBody Cliente cliente) {
 		System.out.println("\n AQUI ESTOU \n"+cliente.toString());
 		return repository.save(cliente);
 	}
+	
+	@DeleteMapping("/cliente/{id}")
+	public void deleteCliente(@PathVariable(name = "id", required = true) Long id) {
+		repository.deleteById(id);
+	}
+	
+	@PutMapping(value="/cliente/{id}")
+	public ResponseEntity update(@PathVariable("id") long id,
+	                                      @RequestBody Cliente cliente) {
+	   return repository.findById(id)
+	           .map(record -> {
+	               record.setNome(cliente.getNome());
+	               record.setEmail(cliente.getEmail());
+	               record.setCpf(cliente.getCpf());
+	               record.setTelefone(cliente.getTelefone());
+	               record.setEndereco(cliente.getEndereco());
+	               Cliente updated = repository.save(record);
+	               return ResponseEntity.ok().body(updated);
+	           }).orElse(ResponseEntity.notFound().build());
+	}
+	
 }
